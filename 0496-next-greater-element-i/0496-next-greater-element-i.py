1669132bd69f3c81e3ee2d22baba2d417ae9eeb6
hamna321 <119731228+hamna321@users.noreply.github.com>
2023-08-07 11:24:43 +0500

    Time: 43 ms (55.04%), Space: 13.3 MB (97.94%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -7,9 +7,13 @@ class Solution(object):
         """
         
         def test(n2,n):
-            for x in n2:
-                if x > n:
-                    return x
+            for i in n2:
+                if i > n:
+                    return i
             return -1
 
         return [ test(nums[nums.index(n)+1:],n) for n in findNums ]
+#For n = 4:
+# The index of 4 in nums is 2.
+# get the sub-list of elements after the index of 4 in nums: [2].
+# Call the test function with the sub-list [2] and n = 4. Since there is no element greater than 4 in [2], the result is -1.
