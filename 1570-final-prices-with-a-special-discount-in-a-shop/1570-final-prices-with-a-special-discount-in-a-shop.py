cb0b33cb422f38a212f21c0a412e16b28db2e5ff
hamna321 <119731228+hamna321@users.noreply.github.com>
2023-08-11 12:55:00 +0500

    Time: 55 ms (85.38%), Space: 16.5 MB (13.51%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,7 +1,7 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         stack = []
-        res = prices.copy()
+        res = prices[:]
         for i in range(len(prices)):
             while stack and prices[i] <= prices[stack[-1]]:
                j = stack.pop()
