d540f92e72af1997e5e5ff9e13727c86c7a41796
hamna321 <119731228+hamna321@users.noreply.github.com>
2023-08-15 12:47:19 +0500

    Time: 209 ms (94.2%), Space: 17.8 MB (92.51%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,8 +1,13 @@
 class Solution:
     def search(self,nums:List[int],target:int)->int:
-        n=len(nums)
-        for i in range(n):
-            if nums[i]==target:
-                return i
-
-        return -1  
+        l = 0
+        r =  len(nums) - 1
+        while l <= r :
+            m = (l + r)  // 2
+            if nums[m] > target:
+                r = m -1
+            elif nums[m] < target:
+                l = m + 1
+            else :
+                return m
+        return -1
