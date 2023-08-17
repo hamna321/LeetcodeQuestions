961eb0224950998f1354af73b955300dd695fc3e
hamna321 <119731228+hamna321@users.noreply.github.com>
2023-08-17 13:44:44 +0500

    Time: 73 ms (98.74%), Space: 23.8 MB (67.81%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.py
@@ -0,0 +1,11 @@
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        codes = [0]
+        offset = 1
+        
+        for i in range(n):
+            for j in range(len(codes)-1, -1, -1):
+                codes.append(codes[j] + offset)
+            offset = offset << 1
+        
+        return codes
