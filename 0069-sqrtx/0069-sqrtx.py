c8bd93635d5f4cb07d5b073f9ee4ebfcf7449ec4
hamna321 <119731228+hamna321@users.noreply.github.com>
2023-08-18 11:08:49 +0500

    Time: 46 ms (75.18%), Space: 16.4 MB (24.82%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -3,11 +3,11 @@ class Solution:
         l=1
         r=x
         while l<=r:
-            mid=(l+r)>>1
+            mid=(l+r)//2
             if mid*mid==x:
                 return mid
             elif mid*mid<x:
-                l=mid+1
+                l =  mid + 1
             else:
-                r=mid-1
-        return l-1
+                r = mid - 1
+        return r
