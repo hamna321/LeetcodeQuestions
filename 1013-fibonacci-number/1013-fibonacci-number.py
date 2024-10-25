83dca152cbeb42540ce278cbe8b5c7835e9f484e
hamna321 <119731228+hamna321@users.noreply.github.com>
2024-10-25 21:21:26 +0500

    Time: 433 ms (37.76%), Space: 11.7 MB (28.73%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,10 +1,26 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        res=0
-        if n==0:
+# base case
+'''
+n == 0 or n == 1 elif case n == 2
+2 - else count for fibonacci using formula
+F(n) = F(n - 1) + F(n - 2)
+'''
+
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
             return 0
-        if n==1:
+        elif n == 1 or n == 2: 
             return 1
         else:
-            res=Solution.fib(self,n-1)+Solution.fib(self,n-2)
+            res = self.fib(n-1) + self.fib(n-2)
+
         return res
+        
+
+        
+
+        
