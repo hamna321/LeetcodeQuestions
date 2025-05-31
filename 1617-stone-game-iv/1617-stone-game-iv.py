00c9bdda0bcfa841dfefad34d210b70aafef5716
hamna321 <119731228+hamna321@users.noreply.github.com>
2025-05-31 12:08:44 +0500

    Time: 1678 ms (26.25%), Space: 48.2 MB (26.81%) - LeetHub

--- 1617-stone-game-iv/1617-stone-game-iv.py
+++ 1617-stone-game-iv/1617-stone-game-iv.py
@@ -1,37 +1,17 @@
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
-     
         memo = {}
-        
+
         def can_win(k):
-            
             if k in memo:
                 return memo[k]
-                
-            
-            root = int(k ** 0.5)
-            if root * root == k:
-                memo[k] = True
-                return True
-                
-            
-            if k == 1:
-                memo[k] = True
-                return True
-            if k == 2:
-                memo[k] = False
-                return False
-                
-            
-            for move in range(1, root + 1):
-                square = move * move
-                if square > k:
-                    break
-                if not can_win(k - square):
+            i = 1
+            while i * i <= k:
+                if not can_win(k - i * i):
                     memo[k] = True
                     return True
-                    
+                i += 1
             memo[k] = False
             return False
-        
+
         return can_win(n)
